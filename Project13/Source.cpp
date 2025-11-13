c2f9b7ec41457c062872d1a006f6ad70aa05d1a5
Artem <Artem@gamil.com>
2025-11-13 19:13:06 +0200

    2312312

--- Project13/Source.cpp
+++ Project13/Source.cpp
@@ -11,7 +11,7 @@ protected:
 	string color;
 	int numberOfSeats;
 public:
-	Transport() {}
+	Transport() { MaxSpeed = 0.0; numberOfSeats = 0; }
 	Transport(string n, double m, string c, int num)
 	{
 		name = n;
@@ -61,7 +61,7 @@ class Truck :public Transport
 	double LiftingCapacity;
 	double Height;
 public:
-	Truck(){}
+	Truck() { LiftingCapacity = 0.0; Height = 0.0; }
 	Truck(string n, double m, string c, int num,double l,double h)
 	{
 		name = n;
@@ -93,9 +93,108 @@ public:
 		cout << "LiftingCapacity: " << LiftingCapacity << "\t Height: " << Height << endl;
 	}
 };
+class Airplane : public Transport
+{
+	double NumberOfHoursInTheAir;
+	double wingspan;
+	int turbines;
+public:
+	Airplane() {}
+	Airplane(string n, double m, string c, int num, double nu, double wing, int t)
+	{
+		name = n;
+		MaxSpeed = m;
+		color = c;
+		numberOfSeats = num;
+		NumberOfHoursInTheAir = nu;
+		wingspan = wing;
+		turbines = t;
+	}
+	void SetNumberOfHoursInTheAir(double nu)
+	{
+		NumberOfHoursInTheAir = nu;
+	}
+	double GetNumberOfHoursInTheAir()
+	{
+		return NumberOfHoursInTheAir;
+	}
+	void Setwingspan(double wing)
+	{
+		wingspan = wing;
+	}
+	double Getwingspan()
+	{
+		return wingspan;
+	}
+	void Setturbines(int t)
+	{
+		turbines = t;
+	}
+	int Getturbines()
+	{
+		return turbines;
+	}
+	void Print()
+	{
+		Transport::Print();
+		cout << "NumberOfHoursInTheAir: " << NumberOfHoursInTheAir << "\t wingspan: " << wingspan << "\t turbines: " << turbines << endl;
+	}
+};
+
+class Vessel : public Transport
+{
+	string FuleType;
+	double NumberOfKnockouts;
+	double UnderwaterWatch;
+public:
+	Vessel() {}
+	Vessel(string n, double m, string c, int num, string f, double kno, double und)
+	{
+		name = n;
+		MaxSpeed = m;
+		color = c;
+		numberOfSeats = num;
+		FuleType = f;
+		NumberOfKnockouts = kno;
+		UnderwaterWatch = und;
+	}
+	void SetFuleType(string f)
+	{
+		FuleType = f;
+	}
+	string GetFuleType()
+	{
+		return FuleType;
+	}
+	void SetNumberOfKnockouts(double kno)
+	{
+		NumberOfKnockouts = kno;
+	}
+	double GetNumberOfKnockouts()
+	{
+		return NumberOfKnockouts;
+	}
+	void SetUnderwaterWatch(double und)
+	{
+		UnderwaterWatch = und;
+	}
+	double GetUnderwaterWatch()
+	{
+		return UnderwaterWatch;
+	}
+	void Print()
+	{
+		Transport::Print();
+		cout << "FuleType: " << FuleType << "\t NumberOfKnockouts: " << NumberOfKnockouts << "\t UnderwaterWatch: " << UnderwaterWatch << endl;
+	}
+};
 
 int main()
 {
-	Truck obj1("ada", 20, "red", 3, 3, 2.5);
-		obj1.Print();
+	Truck obj1("kamaz", 120, "red", 2, 5000, 3.5);
+	obj1.Print();
+	Airplane obj2("boing", 800, "white", 150, 5, 60, 4);
+	obj2.Print();
+	Vessel obj3("titanic", 40, "blue", 2000, "natural gas", 10, 300);
+	obj3.Print();
 }
